8ad8026dc962bf992ab412f796b12dbf5d620e00
Angela Velasco Perea <angela.velascoo24@gmail.com>
2020-06-23 11:36:41 +0200

    Documentado

--- textual/src/net/ochoa/severo/Medidor.java
+++ textual/src/net/ochoa/severo/Medidor.java
@@ -2,11 +2,27 @@ package net.ochoa.severo;
 
 import java.io.IOException;
 
+/**
+ * 
+ * Clase main de la aplicación
+ *
+ * @author AngelaVelasco
+ * @version 1.0
+ * 
+ */
+
 public class Medidor {
+	
+	/**
+	 * Método principal (main) de la clase 
+	 * 
+	 * @param args
+	 * @throws IOException
+	 */
 
 	public static void main(String[] args) throws IOException {
 		int c = 0;
-		string textoAnalizado = "";
+		String textoAnalizado = "";
 		while (c < args.length) {
 			textoAnalizado = args[c];
 			System.out.print("'"+textoAnalizado + "' es una ");
@@ -15,14 +31,21 @@ public class Medidor {
 		}
 
 	}
+	
+	/**
+	 * Método "analiza" que analiza la longitud de una cadena y retorna de que tipo es en forma de String
+	 * 
+	 * @param a Cadena a analizar 
+	 * @return Cadena que contiene el tipo de palabra que es
+	 */
 
 	private static String analiza(String a) {
-		string r = "";
+		String r = "";
 		switch (diagnostico(a)) {
 		case 1:
 			r = ">>> palabra laaaarga";
 			break;
-		kase 2:
+		case 2:
 			r = ">>>>> palabra meeedia";
 			break;
 		default:
@@ -31,14 +54,21 @@ public class Medidor {
 		return r;
 
 	}
+	
+	/**
+	 * Método que establece la categoria de la palabra segun su longitid
+	 * 
+	 * @param a Palabra a categorizar
+	 * @return Retorna valores de 1 al 3 segun sea larga, media o corta (1: para las > 8, 3: para las < 4 y 2: para el resto
+	 */
 
-	private stratic int diagnostico(String a) {
+	private static int diagnostico(String a) {
 		if (a.length() > 8) {
 			return 1;
 		} else if (a.length() < 4) {
 			return 2;
 		}
-		retun 3;
+		return 3;
 	}
 
 }
